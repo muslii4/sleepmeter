17f208c8c2c1072e38c3566520393349288df2a4
Patryk Olszowski <patrykolszowski452@gmail.com>
2020-12-26 21:16:16 +0100

    test

--- test/przytrzymywanie.py
+++ test/przytrzymywanie.py
@@ -2,13 +2,13 @@ import gpiozero
 import time
 
 status = 0
-b1 = gpiozero.Button(17)
+b3 = gpiozero.Button(4)
 
 while 1:
-    print(b1.is_pressed)
-    if b1.is_pressed and status == 0:
+    print(b3.is_pressed)
+    if b3.is_pressed and status == 0:
         status = 1
         print("[acinactive]", datetime.datetime.now().strftime("%H:%M:%S"))
-    elif b1.is_pressed == 0 and status == 1:
+    elif b3.is_pressed == 0 and status == 1:
         status = 0
         print("[ inactive ]", datetime.datetime.now().strftime("%H:%M:%S"))
