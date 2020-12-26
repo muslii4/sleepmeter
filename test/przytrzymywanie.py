01bcee06813380d37e5ca4700aad740cf2cb2c60
Patryk Olszowski <patrykolszowski452@gmail.com>
2020-12-26 21:10:39 +0100

    test

--- test/przytrzymywanie.py
+++ test/przytrzymywanie.py
@@ -5,6 +5,7 @@ status = 0
 b1 = gpiozero.Button(17)
 
 while 1:
+    print(b1.is_pressed)
     if b1.is_pressed and status == 0:
         status = 1
         print("[acinactive]", datetime.datetime.now().strftime("%H:%M:%S"))
